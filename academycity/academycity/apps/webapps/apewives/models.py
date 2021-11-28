2be7a5daef6492831a6d4d1d4f0b010b3cdd96d0
OpioDaniel <danielopio540@gmail.com>
2021-11-28 17:47:31 +0300

    daniel 11/28/2021 apewives/harambe unchained

--- academycity/academycity/apps/webapps/apewives/models.py
+++ academycity/academycity/apps/webapps/apewives/models.py
@@ -8,57 +8,31 @@ class ApewivesWeb(models.Model):
     main_page_twitter_link = models.CharField(max_length=100, default='')
     main_page_discord_image = models.ImageField(upload_to="apewives_web/home/social_media_images", default="no_image.png")
     main_page_discord_link = models.CharField(max_length=100, default='')
-
     main_page_logo_image = models.ImageField(upload_to="apewives_web/home/main_page_image", default="no_image.png")
-
-    logo_image_link = models.CharField(max_length=100, default='')
     text_main_page_title = models.CharField(max_length=100, default='')
 
     text_second_page_title = models.CharField(max_length=256, default='')
-    text_second_page_text = models.CharField(max_length=512, default='')
+    text_second_page_text = models.TextField(blank=True, null=True)
 
-    community_title = models.CharField(max_length=100, default='')
-    community_text = models.CharField(max_length=512, default='')
-    community_twitter_image = models.ImageField(upload_to="apewives_web/home/social_media_images", default="no_image.png")
-    community_discord_image = models.ImageField(upload_to="apewives_web/home/social_media_images", default="no_image.png")
+    apes_out_title = models.CharField(max_length=256, default='')
+    apes_out_text = models.TextField(blank=True, null=True)
 
-    membership_image = models.ImageField(upload_to="apewives_web/home/membership_images", default="no_image.png")
-    membership_title = models.CharField(max_length=100, default='')
-    membership_text = models.CharField(max_length=512, default='')
-
-    buy_title = models.CharField(max_length=100, default='')
-    buy_text = models.CharField(max_length=512, default='')
-    buy_image = models.ImageField(upload_to="apewives_web/home/buy_images", default="no_image.png")
-    buy_link = models.CharField(max_length=100, default='')
-
+    # comics bridge
     specs_image = models.ImageField(upload_to="apewives_web/home/specs_images", default="no_image.png")
     specs_title = models.CharField(max_length=100, default='')
     specs_text_paragraph = models.TextField(blank=True, null=True)
 
+    # Meta Universe
     ownership_image = models.ImageField(upload_to="apewives_web/home/ownership_images", default="no_image.png")
     ownership_title = models.CharField(max_length=100, default='')
     ownership_text_paragraph = models.TextField(blank=True, null=True)
 
     roadmap_image = models.ImageField(upload_to="apewives_web/home/roadmap_images", default="no_image.png")
     roadmap_title = models.CharField(max_length=100, default='')
-    roadmap_text = models.CharField(max_length=512, default='')
-
-    member_tool_title = models.CharField(max_length=100, default='')
-    member_tool_text = models.CharField(max_length=512, default='')
-    member_rarity_tools = models.CharField(max_length=100, default='')
-    member_rarity_tools_link = models.CharField(max_length=100, default='')
 
     team_title = models.CharField(max_length=100, default='')
-    team_text = models.CharField(max_length=512, default='')
+    team_text = models.TextField(blank=True, null=True)
 
-    about_title = models.CharField(max_length=100, default='')
-
-    about_text_top = models.TextField(blank=True, null=True)
-    about_text_bottom = models.TextField(blank=True, null=True)
-    contract_address_text_paragraph = models.TextField(blank=True, null=True)
-
-    footer_title_1 = models.CharField(max_length=100, default='')
-    footer_title_2 = models.CharField(max_length=100, default='')
     footer_desc = models.CharField(max_length=100, default='')
 
     youtube_image = models.ImageField(upload_to="apewives_web/home/social_media_images", default="no_image.png")
@@ -79,7 +53,7 @@ class TitleSlides(models.Model):
 
     order = models.PositiveSmallIntegerField(default=0)
     apewives = models.ForeignKey(ApewivesWeb, null=True, on_delete=models.CASCADE, related_name='titleslides')
-    text = models.CharField(max_length=512, default='', blank=True)
+    text = models.TextField(blank=True, null=True)
 
 
 class SlidingImages(models.Model):
@@ -104,11 +78,11 @@ class Roadmap(models.Model):
 
     order = models.PositiveSmallIntegerField(default=0)
     apewives = models.ForeignKey(ApewivesWeb, null=True, on_delete=models.CASCADE, related_name='roadmaps')
-    text = models.CharField(max_length=512, default='', blank=True)
+    text = models.TextField(blank=True, null=True)
     link = models.CharField(max_length=100, null=True, blank=True)
     link_name = models.CharField(max_length=100, default='', blank=True)
     roadmap_data_id = models.CharField(max_length=100, default='', blank=True)
-    roadmap_timeline_item_point = models.IntegerField(default=0)
+    roadmap_timeline_item_point = models.IntegerField(default=10)
 
 
 class Team(models.Model):
